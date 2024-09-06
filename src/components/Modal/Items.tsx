069b1bd605f49d6d253cb3270ed66323a55b933f
Olaleye Igbagbo <89357459+igee-17@users.noreply.github.com>
2024-09-06 21:14:24 +0100

    modal text adjusted

--- src/components/Modal/Items.tsx
+++ src/components/Modal/Items.tsx
@@ -3,17 +3,15 @@ import React from "react";
 const Items: React.FC<ItemsProps> = ({ heading, text, amount, num, total }) => {
   return (
     <div className="h-[88px] w-full bg-white justify-start items-center gap-3 md:gap-6 inline-flex">
-      <div className="grow shrink basis-0 e h-[88px] py-4 bg-white justify-start items-center gap-2.5 flex">
-        <div className="grow shrink basis-0 flex-col justify-start items-start gap-1 inline-flex">
-          <h2 className="text-[#1f1f23] text-[10px] md:text-xs lg:text-base font-normal font-['Neue Haas Grotesk Text Pro']">
-            {heading}
-          </h2>
-          {text && (
-            <p className="self-stretch text-[#666f77] text-[10px] md:text-[13px] font-normal font-['Neue Haas Grotesk Text Pro']">
-              {text}
-            </p>
-          )}
-        </div>
+      <div className="w-fit lg:w-[252px] flex-col justify-start items-start gap-1 inline-flex">
+        <h2 className="text-[#1f1f23] text-[10px] md:text-xs lg:text-base font-normal font-['Neue Haas Grotesk Text Pro']">
+          {heading}
+        </h2>
+        {text && (
+          <p className="self-stretch text-[#666f77] text-[10px] md:text-[13px] font-normal font-['Neue Haas Grotesk Text Pro']">
+            {text}
+          </p>
+        )}
       </div>
       <div className="justify-start  items-center gap-1 md:gap-2 lg:gap-4 flex">
         <div className="w-[20px] mb:w-[30px] md:w-[50px] lg:w-[75px] self-stretch py-4 bg-white justify-center items-center gap-2.5 flex">
